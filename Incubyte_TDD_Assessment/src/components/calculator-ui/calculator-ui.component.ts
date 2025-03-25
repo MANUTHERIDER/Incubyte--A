26b54f5d6bd4a1fd7452ae26bd92edb172ce7446
Manu Sahu <sahumanu021@gmail.com>
2025-03-25 20:42:42 +0530

    Committhe component

---
+++ Incubyte_TDD_Assessment/src/components/calculator-ui/calculator-ui.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-calculator-ui',
+  imports: [],
+  templateUrl: './calculator-ui.component.html',
+  styleUrl: './calculator-ui.component.css'
+})
+export class CalculatorUIComponent {
+
+}
